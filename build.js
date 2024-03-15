4a7a8204da092afb45737e88cc3b481ef8aa81dd
Dominic <dom@scameo.xyz>
2024-03-15 17:21:47 +0000

    Mass Dump

--- build.js
+++ build.js
@@ -5,6 +5,8 @@ var htmlFiles = fs.readdirSync(folder).filter(i => i.endsWith('.html'))
 fs.rmSync("output", { recursive: true, force: true })
 fs.mkdirSync("output")
 fs.copyFileSync("src/303 Graphics.mp4", "output/303 Graphics.mp4")
+fs.copyFileSync("src/StyleSheet.css", "output/StyleSheet.css")
+
 
 var footerContents = fs.readFileSync("src/partials/footer.html", 'utf8')
 var headerContents = fs.readFileSync("src/partials/header.html", 'utf8')
